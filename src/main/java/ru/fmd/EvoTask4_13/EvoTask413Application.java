ba1e88c7e5c2e576b34df7670c4bdb144498ff33
andrei_chudotvorov <lonelywolf_91@mail.ru>
2025-03-05 10:30:56 +0700

    Task 4_13 init

---
+++ src/main/java/ru/fmd/EvoTask4_13/EvoTask413Application.java
@@ -0,0 +1,13 @@
+package ru.fmd.EvoTask4_13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoTask413Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoTask413Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/fmd/EvoTask4_13/EvoTask413ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fmd.EvoTask4_13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvoTask413ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
